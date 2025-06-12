6c2f08693b8d688d9685737328a2d31caf29993a
Damian Klata <costam123987@gmail.com>
2025-06-12 18:53:57 +0200

    persist selected sport

--- assets/stores/sports.js
+++ assets/stores/sports.js
@@ -37,6 +37,6 @@ export const useSportsStore = defineStore('sports', () => {
     return {sports, fetchSports, selectSport, selectedSport, isLoading, errorMessage}
 }, {
     persist: {
-        paths: [''], // only persist the data, not loading/error
+        paths: ['selectedSport'], // only persist the data, not loading/error
     }
 })
