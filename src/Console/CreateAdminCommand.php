2d5869db0d0d8f31298dfb331b69d3fb8a4cd1ca
Damian Klata <costam123987@gmail.com>
2025-06-30 15:00:24 +0200

    fixtures added and readme file

---
+++ src/Console/CreateAdminCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\Interface\UserRepositoryInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Creates a new admin user.',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly UserRepositoryInterface $userRepository
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $helper = $this->getHelper('question');
+
+        $io->title('Admin User Creator');
+
+        $emailQuestion = new Question('Please enter the admin email: ');
+        $email = $helper->ask($input, $output, $emailQuestion);
+
+        $passwordQuestion = new Question('Please enter the password: ');
+        $passwordQuestion->setHidden(true);
+        $passwordQuestion->setHiddenFallback(false);
+        $plainPassword = $helper->ask($input, $output, $passwordQuestion);
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, $plainPassword)
+        );
+
+        $this->userRepository->save($user, true);
+
+        $io->success(sprintf('Admin user %s successfully created!', $email));
+
+        return Command::SUCCESS;
+    }
+}
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,28 +4,32 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements FixtureGroupInterface
 {
+    public static function getGroups(): array
+    {
+        return ['test-data'];
+    }
     public function __construct(
         private readonly UserPasswordHasherInterface $passwordHasher
-    )
-    {
+    ) {
     }
 
     public function load(ObjectManager $manager): void
     {
         $adminUser = new User();
-        $adminUser->setEmail('admin@wp.pl');
+        $adminUser->setEmail('admin@test.com');
         $adminUser->setPassword($this->passwordHasher->hashPassword($adminUser, 'password123'));
         $adminUser->setRoles(['ROLE_ADMIN']);
 
         $manager->persist($adminUser);
 
         $user = new User();
-        $user->setEmail('user@wp.pl');
+        $user->setEmail('user@test.com');
         $user->setPassword($this->passwordHasher->hashPassword($user, 'password123'));
         $user->setRoles(['ROLE_USER']);
 
