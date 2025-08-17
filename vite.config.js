ddaf940f7d0b428487f9b846843f0e34c13f5ed3
Damian Klata <costam123987@gmail.com>
2025-08-17 23:46:00 +0200

    try

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         symfonyPlugin(),
         vue(),
     ],
-    base: '/build/',
+    base: '/',
     build: {
         outDir: 'public/build',
         rollupOptions: {
